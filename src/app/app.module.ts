e5175bbf9086fe4e389879092ffa6a9090007333
nnymhadora <janagracanin@gmail.com>
2023-07-30 20:57:32 +0200

    added helpers and pages modules, finished home page html css and header html css, still need to do user management/routing. footer not finished html css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HelpersModule } from './components/helpers/helpers.module';
+import { PagesModule } from './components/pages/pages.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HelpersModule,
+    PagesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/pages/pages.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [CommonModule, PagesRoutingModule],
+  exports: [],
+})
+export class PagesModule {}
